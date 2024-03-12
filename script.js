915af952b7083ad75c7f84bfe9dfc0cf89db372a
MichaelHowe11 <michael.howell23@student.cart.org>
2024-03-11 21:32:05 -0700

    update

--- script.js
+++ script.js
@@ -1,99 +1,63 @@
-
 let totalExpenses = 0;
+
 function updateMonthIncome(stTax1) {
     let yearSalary = document.getElementById('catDrop').value;
     console.log(yearSalary)
-    
+    let mortgage = parseFloat(document.getElementById('mortgage').value) || 0;
+    let rent = parseFloat(document.getElementById('rent').value) || 0;
+    let homeInsurance = parseFloat(document.getElementById('homeInsurance').value) || 0;
+    let repairsMaintenance = parseFloat(document.getElementById('repairsMaintenance').value) || 0;
+    let utilities = parseFloat(document.getElementById('utilities').value) || 0;
+    let cableTVInternet = parseFloat(document.getElementById('cableTVInternet').value) || 0;
+    let phoneBill = parseFloat(document.getElementById('phoneBill').value) || 0;
+    let carPayment = parseFloat(document.getElementById('carPayment').value) || 0;
+    let carInsurance = parseFloat(document.getElementById('carInsurance').value) || 0;
+    let gasFuel = parseFloat(document.getElementById('gasFuel').value) || 0;
+    let carRepairs = parseFloat(document.getElementById('carRepairs').value) || 0;
+    let collegeTuition = parseFloat(document.getElementById('collegeTuition').value) || 0;
+    let studentLoans = parseFloat(document.getElementById('studentLoans').value) || 0;
+    let groceries = parseFloat(document.getElementById('groceries').value) || 0;
+    let clothing = parseFloat(document.getElementById('clothing').value) || 0;
+    let entertainment = parseFloat(document.getElementById('entertainment').value) || 0;
+    let medical = parseFloat(document.getElementById('medical').value) || 0;
+    let petExpenses = parseFloat(document.getElementById('petExpenses').value) || 0;
+    let otherExpenses = parseFloat(document.getElementById('otherExpenses').value) || 0;
+    let savings = parseFloat(document.getElementById('savings').value) || 0;
+    let investments = parseFloat(document.getElementById('investments').value) || 0;
+    let retirement = parseFloat(document.getElementById('retirement').value) || 0;
 
+    totalExpenses = rent + homeInsurance + repairsMaintenance + utilities + cableTVInternet + phoneBill + carPayment + carInsurance + gasFuel + carRepairs + collegeTuition + studentLoans + groceries + clothing + entertainment + medical + petExpenses + otherExpenses + savings + investments + retirement + mortgage;
 
-
-
-    let rent = parseFloat(document.getElementById('rent').value) || 0
-    console.log(yearSalary)
-
-    let homeInsurance = parseFloat(document.getElementById('homeInsurance').value) || 0
-    console.log(homeInsurance)
-
-    let repairsMaintenance = parseFloat(document.getElementById('repairsMaintenance').value) || 0
-    console.log(repairsMaintenance)
-
-    let utilities = parseFloat(document.getElementById('utilities').value) || 0
-    console.log(utilities)
-
-    let cableTVInternet = parseFloat(document.getElementById('cableTVInternet').value) || 0
-    console.log(cableTVInternet)
-
-    let phoneBill = parseFloat(document.getElementById('phoneBill').value) || 0
-    console.log(phoneBill)
-
-    let carPayment = parseFloat(document.getElementById('carPayment').value) || 0
-    console.log(carPayment)
-
-    let carInsurance = parseFloat(document.getElementById('carInsurance').value) || 0
-    console.log(carInsurance)
-
-    let gasFuel = parseFloat(document.getElementById('gasFuel').value) || 0
-    console.log(gasFuel)
-
-    let carRepairs = parseFloat(document.getElementById('carRepairs').value) || 0
-    console.log(carRepairs)
-
-    let collegeTuition = parseFloat(document.getElementById('collegeTuition').value) || 0
-    console.log(collegeTuition)
-
-    let studentLoans = parseFloat(document.getElementById('studentLoans').value) || 0
-    console.log(studentLoans)
-
-    let groceries = parseFloat(document.getElementById('groceries').value) || 0
-    console.log(groceries)
-
-    let clothing = parseFloat(document.getElementById('clothing').value) || 0
-    console.log(clothing)
-
-    let entertainment = parseFloat(document.getElementById('entertainment').value) || 0
-    console.log(entertainment)
-
-    let medical = parseFloat(document.getElementById('medical').value) || 0
-    console.log(medical)
-
-    let petExpenses = parseFloat(document.getElementById('petExpenses').value) || 0
-    console.log(petExpenses)
-
-    let otherExpenses = parseFloat(document.getElementById('otherExpenses').value) || 0
-    console.log(otherExpenses)
-
-    let savings = parseFloat(document.getElementById('savings').value) || 0
-    console.log(savings)
-
-    let investments = parseFloat(document.getElementById('investments').value) || 0
-    console.log(investments)
-
-    let retirement = parseFloat(document.getElementById('retirement').value) || 0
-    console.log(retirement)
-
-
-
-
-
-
-    totalExpenses = rent + homeInsurance + repairsMaintenance + utilities + cableTVInternet + phoneBill + carPayment + carInsurance + gasFuel + carRepairs + collegeTuition + studentLoans + groceries + clothing + entertainment + medical + petExpenses + otherExpenses + savings + investments + retirement;
-
-    yearSalary = yearSalary.replace(/\,/g, '')
-
-    console.log((yearSalary))
+    yearSalary = yearSalary.replace(/\,/g, '');
 
     let monthIncome = 0;
 
     let customInput = document.getElementById("userInput").value;
-    console.log(customInput)
-    monthIncome = ((customInput / 12) - totalExpenses).toFixed(2) || (parseInt(yearSalary) / 12 - totalExpenses).toFixed(2);
-    console.log(monthIncome)
 
-    monthIncomeAft.innerHTML = "Monthly Income After Expenses: $" + monthIncome;
+    if (customInput != "") {
+        customInput = parseFloat(customInput);
+        monthIncome = (customInput / 12) * (1 - parseFloat(document.getElementById('usStates').value)) - totalExpenses;
+        monthIncome = monthIncome || 0;
+    } else {
+        monthIncome = (parseFloat(yearSalary) / 12) * (1 - parseFloat(document.getElementById('usStates').value)) - totalExpenses;
+    }
+    monthIncomeAft.innerHTML = "Monthly Income After Expenses: $" + monthIncome.toFixed(2)||yearSalary.toFixed(2);
+
+    let monthIncome2 = monthIncome;
+
+    let fedTax = 0.07*monthIncome
+    let socSec = 0.062*monthIncome
+    let med = 0.0145*monthIncome
+    let stDis = 0.01*monthIncome
+    let retInv = 0.05*monthIncome
+
+    monthIncome = monthIncome - (fedTax+socSec+med+stDis+retInv+180)
+
+    yearSalary = yearSalary - (fedTax+socSec+med+stDis+retInv+180)
 
 
 
-
+    total.innerHTML = "Total Budget: $" + monthIncome.toFixed(2)||yearSalary.toFixed(2);
 }
 
 document.addEventListener(`DOMContentLoaded`, function () {
@@ -101,57 +65,35 @@ document.addEventListener(`DOMContentLoaded`, function () {
     let monthIncome = 0;
     let stateTaxRate = 0;
 
-
     document.getElementById('userInput').addEventListener('change', function (eventData) {
         updateMonthIncome();
-
-        yearSalary = parseFloat(document.getElementById('userInput').value);
+        yearSalary = (parseFloat(document.getElementById('userInput').value));
     });
 
     document.getElementById('usStates').addEventListener('change', function (eventData) {
         stateTaxRate = parseFloat(document.getElementById('usStates').value);
-        stTax.innerHTML = "State Taxes: " + (stateTaxRate * 100).toFixed(3) + "%"
+        stTax.innerHTML = "State Taxes: " + ((stateTaxRate * 100).toFixed(3)) || document.getElementById('catDrop').value + "%";
         yearSalary = yearSalary * (1 - stateTaxRate);
-
-        total.innerHTML = "Total Budget: " + (0)
+        total.innerHTML = "Total Budget: $" + (yearSalary || document.getElementById('catDrop').value);
         updateMonthIncome(stateTaxRate);
     });
 
-
-
-
-
-
     document.getElementById("submitButton").addEventListener("submit", function (eventData) {
-        updateMonthIncome()
-
-        monthIncome = (monthIncomeAft.innerHTML).replace(/[^\d.-]/g, '')
-        console.log(totalExpenses)
+        updateMonthIncome();
+        stateTaxRate = parseFloat(document.getElementById('usStates').value);
+        monthIncome = parseFloat(monthIncomeAft.innerHTML.replace(/[^\d.-]/g, '')) || 0;
         eventData.preventDefault();
-
-
-
-
-        console.log(stateTaxRate)
-
-        console.log(monthIncome)
     });
-
 
     function expenFunction() {
-        updateMonthIncome()
+        updateMonthIncome();
     }
 
-
-
-
     document.getElementById('expen1').addEventListener('change', function (eventData) {
-        updateMonthIncome()
-        console.log(monthIncome)
-    });
-    document.getElementById('expen2').addEventListener('change', function (eventData) {
-        updateMonthIncome()
-        console.log(monthIncome)
+        updateMonthIncome();
     });
 
+    document.getElementById('expen2').addEventListener('change', function (eventData) {
+        updateMonthIncome();
+    });
 });
